996c2b8eada1b4dcda632e5c3cab0f26accc0bf5
Seyfadin Abdela <seyfadinabdela42@gmail.com>
2024-12-12 17:04:21 +0300

    Create Printing.cmd

---
+++ 1273-Seyfadin-Abdela/Activity 3.1/Printing.cmd
@@ -0,0 +1,33 @@
+
+
+Flowchart Explanation
+
+I'll provide a flowchart describing the overall logic:
+
+1. Start.
+
+
+2. Accept user input n for the dimension.
+
+
+3. Display patterns in the following sequence:
+
+Full Pyramid.
+
+Inverted Half Pyramid.
+
+Inverted Full Pyramid.
+
+Hollow Inverted Half Pyramid.
+
+Hollow Full Pyramid.
+
+
+
+4. End.
+
+
+
+Let
+
+  
