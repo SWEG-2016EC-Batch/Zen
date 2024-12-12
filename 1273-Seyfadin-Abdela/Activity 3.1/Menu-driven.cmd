ed39cd306bfb4e6c3a001655eba77bc956712101
Seyfadin Abdela <seyfadinabdela42@gmail.com>
2024-12-12 16:57:58 +0300

    Create Menu-driven.cmd

---
+++ 1273-Seyfadin-Abdela/Activity 3.1/Menu-driven.cmd
@@ -0,0 +1,38 @@
+Flow Design (Short)
+
+1. Start
+
+
+2. Input: Integer number from the user
+
+
+3. Display Menu: List all 9 options
+
+
+4. Choice Handling:
+
+If choice = 1: Reverse the number
+
+If choice = 2: Count the digits
+
+If choice = 3: Product of even digits
+
+If choice = 4: Print first & last digit and their sum
+
+If choice = 5: Swap first & last digit
+
+If choice = 6: Check if palindrome
+
+If choice = 7: Frequency of each digit (print as table)
+
+If choice = 8: Check if strong number
+
+If choice = 9: Check if perfect number
+
+
+
+5. Loop: Repeat menu until user chooses to exit
+
+
+6. End
+  
