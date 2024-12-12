7c46e42a80772844a7e7d9c6c818bd127b1f4c83
Seyfadin Abdela <seyfadinabdela42@gmail.com>
2024-12-12 16:49:20 +0300

    Create Netpay.cmd

---
+++ 1273-Seyfadin-Abdela/Activity 3.1/Netpay.cmd
@@ -0,0 +1,40 @@
+Flow Design (Short)
+
+1. Start
+
+
+2. Input: Basic Salary, Worked Hours, Bonus Rate
+
+
+3. If Worked Hours > 40, Input Overtime Bonus Rate
+
+
+4. Calculate:
+
+Extra Hours = Worked Hours - 40 (if any)
+
+Bonus = Extra Hours × Bonus Rate
+
+Gross Salary = Basic Salary + Bonus
+
+Pension = Basic Salary × 0.07
+
+Determine Tax Rate (based on Gross Salary)
+
+Tax = Gross Salary × Tax Rate
+
+
+
+5. Calculate:
+
+Net Salary = Gross Salary - Pension - Tax
+
+
+
+6. Output: Display Net Salary
+
+
+7. End
+
+
+
