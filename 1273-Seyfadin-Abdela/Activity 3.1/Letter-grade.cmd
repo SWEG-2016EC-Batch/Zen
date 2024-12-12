ef451f6a2157e14720e6fcf109df9f083e3b92bd
Seyfadin Abdela <seyfadinabdela42@gmail.com>
2024-12-12 16:55:27 +0300

    Create Letter-grade.cmd,

---
+++ 1273-Seyfadin-Abdela/Activity 3.1/Letter-grade.cmd
@@ -0,0 +1,36 @@
+Flow Design (Short)
+
+1. Start
+
+
+2. Input: Marks for Test, Quiz, Project, Assignment, and Final Exam
+
+
+3. Calculate:
+
+Total Mark = (Test × 0.15) + (Quiz × 0.05) + (Project × 0.20) + (Assignment × 0.10) + (Final Exam × 0.50)
+
+
+
+4. Determine Grade:
+
+If Total Mark ≥ 90, Grade = A+
+
+Else If 80 ≤ Total Mark < 90, Grade = A
+
+Else If 75 ≤ Total Mark < 80, Grade = B+
+
+Else If 60 ≤ Total Mark < 75, Grade = B
+
+Else If 45 ≤ Total Mark < 60, Grade = C
+
+Else If 30 ≤ Total Mark < 45, Grade = D
+
+Else, Grade = F
+
+
+
+5. Output: Display Total Mark and Corresponding Letter Grade
+
+
+6. End
