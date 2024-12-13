b9d01229df20ede7ed51e16e5229f00ab455246d
Zemedkun Workalem <zemedkun.workalem@aastustudent.edu.et>
2024-12-13 20:46:07 +0300

    Update pattern 4b3.cpp

--- ETS1557_ZEMEDKUN_WORKALEM/pattern 4b3.cpp
+++ ETS1557_ZEMEDKUN_WORKALEM/pattern 4b3.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n = 5; 
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= n; col++) {
+            cout << char('a' + col - 1);
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
