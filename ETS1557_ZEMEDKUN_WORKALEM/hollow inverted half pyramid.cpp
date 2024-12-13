9adcbc261122aec5e31829ef5c4b8509c28b80d3
Zemedkun Workalem <zemedkun.workalem@aastustudent.edu.et>
2024-12-13 20:56:33 +0300

    Update hollow inverted half pyramid.cpp

--- ETS1557_ZEMEDKUN_WORKALEM/hollow inverted half pyramid.cpp
+++ ETS1557_ZEMEDKUN_WORKALEM/hollow inverted half pyramid.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n = 6; // Fixed size of the pyramid
+
+    for (int row = n; row >= 1; row--) {
+        for (int col = 1; col <= row; col++) {
+            // Print stars only on the first row or at the borders
+            if (col == 1 || col == row || row == n) {
+                cout << "*";
+            } else {
+                cout << " "; // Print space for hollow effect
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
