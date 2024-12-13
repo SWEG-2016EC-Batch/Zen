6cb341bbe3300c07e8c3df1a0aa5b161e4bc36ae
Zemedkun Workalem <zemedkun.workalem@aastustudent.edu.et>
2024-12-13 21:00:27 +0300

    Update inverted pyramid.cpp

--- ETS1557_ZEMEDKUN_WORKALEM/inverted pyramid.cpp
+++ ETS1557_ZEMEDKUN_WORKALEM/inverted pyramid.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n = 6; // Fixed size of the pyramid
+
+    for (int row = n; row >= 1; row--) {
+        // Print leading spaces
+        for (int col = 1; col <= n - row; col++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int col = 1; col <= row; col++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
