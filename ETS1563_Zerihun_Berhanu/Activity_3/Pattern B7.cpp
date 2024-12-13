5af0b184341fef2b57a7e9182800434761773596
Zerihun Berhanu <zerihun.berhanu@aastustudent.edu.et>
2024-12-13 22:29:01 +0300

    Add files via upload

---
+++ ETS1563_Zerihun_Berhanu/Activity_3/Pattern B7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+int row;
+cout<<"Enter row: ";
+cin>>row;
+for (int i = 1; i <= row; i++){
+for (int k = 1; k <= row-i; k++){
+cout<<" ";
+}
+for (int j = i; 0 < j; j--){
+cout<<"* ";
+}
+cout<<endl;
+}
+return 0;
+}
